0dfd68064e6b253c6a1f62b4882f7e402749ed03
Frarfansjidk <132325891+Smochovhhrdsez@users.noreply.github.com>
2025-08-18 05:09:42 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 sleep 5s;
 
 docker run --rm -i \
-  --shm-size 8g \
+  --shm-size 10g \
   ghcr.io/kishorkumartv000/9czqu-fmr-aio-beta-bot-3sfjx-apple-music-only:forinstantuse
 
 # start the script
