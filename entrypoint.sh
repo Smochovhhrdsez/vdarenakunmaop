c2b18defeaab743f1522a2ad1d4a1818e010021a
Frarfansjidk <132325891+Smochovhhrdsez@users.noreply.github.com>
2025-12-01 11:25:16 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 sleep 5s
 
 docker run --rm -i \
-  ghcr.io/kishorkumartv000/9czqu-fmr-aio-beta-bot-3sfjx-apple-music-only:forinstantuse
+  ghcr.io/tvkk0539/ghcr.io/tvkk0539/x0539amdx0539-main-for-all-general-purpose:forinstantuse01
 
 # Start the main command
 exec "$@"
